9aa5ae07023ae5d31ea0e31b95c11261a74096f3
Ajay Dhar <ajaydhar108@gmail.com>
2026-09-07 07:42:41 -0400

    feat: add Film Score Radio cinematic soundtrack player

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 <footer class="site-footer">
     &copy; <?= date('Y') ?> Book to Screen ·
+    <a href="/film-score-radio/" target="_blank">Film Score Radio ✦</a> ·
     <a href="/admin/">Editorial Administration</a>
 </footer>
